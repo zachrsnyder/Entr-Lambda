bfea460e857b688ac5e8cb79077580595e2cf7ca
zachrsnyder <zachrsnyder2004@gmail.com>
2025-09-20 15:45:36 -0500

    Initialized SAM template and set up placeholder lambda.

---
+++ entr-lambda/src/common/users/getUsers.mjs
@@ -0,0 +1,8 @@
+// placeholder lambda for getting a user from the database.
+
+export async function handleGetUsers(event) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify("Hello world")
+    }
+}
